b5bb9e04b918e96850f79d9de715ca72213441f5
ayushnaik <ayush@onelabventures.com>
2025-06-27 14:56:54 +0530

    comment

--- wise_backend/urls.py
+++ wise_backend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path('api/', include('core.urls')),  # replace 'app' with your actual app name
     path('accounts/', include('accounts.urls')),
 ]
