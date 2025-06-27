15c7cca0c111a20329956d2c5488b1c34bdc79d5
venkatesh.sunke <venkatesh.sunke@onelabventures.com>
2025-06-27 18:14:09 +0530

    bug issue

--- wise_backend/settings.py
+++ wise_backend/settings.py
@@ -223,10 +223,7 @@ AUTH_USER_MODEL = "accounts.VideoAgentUser"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "accounts.authentication.Auth0JWTAuthentication",
-    )
-}
-# DRF Pagination settings
-REST_FRAMEWORK = {
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
